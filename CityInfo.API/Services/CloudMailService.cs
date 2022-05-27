095e482fceb8f245531f42132fe85e72a82ac6d7
Jian Xue <royjianxue@yahoo.com>
2022-05-27 01:02:54 -0400

    Adding Appsetting for MailServices

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -2,10 +2,14 @@
 {
     public class CloudMailService:IMailService
     {
+        private readonly string _mailTo = String.Empty;
+        private readonly string _mailFrom = String.Empty;
 
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
-
+        public CloudMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
         public void Send(string subject, string message)
         {
             //send mail -out to console window
